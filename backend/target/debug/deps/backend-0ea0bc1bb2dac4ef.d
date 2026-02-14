cf4d0ff204362abce715d3117915ae5610ed11e9
Itz-Agasta <rupamgolui69@gmail.com>
2026-02-14 23:46:19 +0530

    Init rust backend

---
+++ backend/target/debug/deps/backend-0ea0bc1bb2dac4ef.d
@@ -0,0 +1,5 @@
+/home/agasta/github/projects/keryx/backend/target/debug/deps/backend-0ea0bc1bb2dac4ef.d: src/main.rs
+
+/home/agasta/github/projects/keryx/backend/target/debug/deps/libbackend-0ea0bc1bb2dac4ef.rmeta: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/backend-84ba45ed31c426b4.d
@@ -0,0 +1,5 @@
+/home/agasta/github/projects/keryx/backend/target/debug/deps/backend-84ba45ed31c426b4.d: src/main.rs
+
+/home/agasta/github/projects/keryx/backend/target/debug/deps/libbackend-84ba45ed31c426b4.rmeta: src/main.rs
+
+src/main.rs:
