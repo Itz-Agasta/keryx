3547a5a2b734ea0a8081926cc99bd318ddc5ffe7
Itz-Agasta <rupamgolui69@gmail.com>
2026-02-16 22:25:46 +0530

    add pg client impl

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod pg;
+
+use pg::PgClient;
+
+#[tokio::main]
+async fn main() -> Result<(), tokio_postgres::Error> {
+    let client = PgClient::connect("host=localhost user=agasta dbname=nominatim").await?;
+
+    let tables = client.show_tables().await?;
+
+    for table in tables {
+        println!("{}", table);
+    }
+
+    Ok(())
 }
---
+++ backend/src/pg.rs
@@ -0,0 +1,40 @@
+use tokio_postgres::{Client, Error, NoTls};
+
+// I will add more later. I may need derive later
+pub struct PgClient {
+    client: Client,
+}
+
+impl PgClient {
+    pub async fn connect(connection_string: &str) -> Result<Self, Error> {
+        let (client, connection) = tokio_postgres::connect(connection_string, NoTls).await?;
+
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+
+        Ok(PgClient { client })
+    }
+    
+    pub async fn show_tables(&self) -> Result<Vec<String>, Error> {
+        let rows = self
+            .client
+            .query(
+                "SELECT tablename
+                FROM pg_tables
+                WHERE schemaname = 'public';",
+                &[],
+            )
+            .await?;
+
+        let mut tables = Vec::new();
+        for row in rows {
+            let tablename: &str = row.get(0);
+            tables.push(tablename.to_string());
+        }
+
+        Ok(tables)
+    }
+}
